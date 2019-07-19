184846b92310e1fb20527054203d96921f7d9020
Lin Qi <qi-lin@outlook.com>
2019-07-18 21:08:02 -0500

    add comment and like at backend

--- Insta/models.py
+++ Insta/models.py
@@ -6,7 +6,21 @@ from django.contrib.auth.models import AbstractUser
 
 from django.urls import reverse
 # Create your models here.
+class InstaUser(AbstractUser):
+    profile_pic = ProcessedImageField(
+        upload_to = 'static/images/profiles',
+        format = 'JPEG',
+        blank = True,
+        null = True,
+    )
 class Post(models.Model):
+    author = models.ForeignKey(
+	        InstaUser,
+	        on_delete=models.CASCADE,
+	        related_name='posts',
+	        blank = True,
+	        null = True,
+	    )
     title = models.TextField(blank=True, null=True)
     image = ProcessedImageField(
         upload_to = 'static/images/posts',
@@ -16,11 +30,41 @@ class Post(models.Model):
         
     def get_absolute_url(self):
 	        return reverse("post_detail", args=[str(self.id)])
+    def get_like_count(self):
+	        return self.likes.count()
+	
+    def get_comment_count(self):
+	    return self.comments.count()
 
-class InstaUser(AbstractUser):
-	    profile_pic = ProcessedImageField(
-	        upload_to = 'static/images/profiles',
-	        format = 'JPEG',
-	        blank = True,
-	        null = True,
+class Comment(models.Model):
+	    post = models.ForeignKey(
+	        Post,
+	        on_delete=models.CASCADE,
+	        related_name='comments')
+	    user = models.ForeignKey(
+	        InstaUser,
+	        on_delete=models.CASCADE,
+	        related_name='comments'
 	    )
+	    comment = models.CharField(max_length=140)
+	    posted_on = models.DateTimeField(
+	        auto_now_add=True,
+	        editable=False
+	    )
+	
+	    def __str__(self):
+	        return self.comment
+	
+class Like(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='likes')
+    user = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name='likes'
+    )
+
+    def __str__(self):
+        return 'Like: ' + self.user.username + ' Likes ' + self.post.title 
--- Insta/views.py
+++ Insta/views.py
@@ -42,3 +42,4 @@ class SignupView(CreateView):
 	form_class = CustomUserCreationForm
 	template_name = 'registration/signup.html'
 	success_url = reverse_lazy('login')
+
